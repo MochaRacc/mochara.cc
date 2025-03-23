f50e66f2401daef02d3cb205c44ea72ed75b8106
Mocha The Raccoon <jackson.z.davis@icloud.com>
2025-03-23 11:29:33 +1100

    domain swapover

--- test/assets/js/script.js
+++
@@ -1,3 +0,0 @@
-$(".card").on("webkitAnimationEnd mozAnimationEnd msAnimationEnd oAnimationEnd animationEnd", (e) => {
-    $(e.target).addClass("grow-finished"); // $(this).addClass(...) doesnt work for some reason
-});
